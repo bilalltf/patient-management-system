2b8a76160d675f2be6308f124b27c6d4c7189a15
Bilal Lotfi <bilal.lotfi2016@gmail.com>
2023-02-13 01:52:02 +0100

    update

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("root","admin","Abc@123456789","patients");
+$con = mysqli_connect("localhost","root","Abc@123456789","patients");
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
